80d9e1b825fca82ca464bf8d2977d10d591c408f
Charlie Zhang <luoyang9@gmail.com>
2017-03-19 23:16:35 -0400

    new accent color and styling

--- js/index.js
+++ js/index.js
@@ -30,14 +30,10 @@ $( document ).ready( () => {
 		$(".nav-init").remove();
 		$(".nav-pulse").remove();
 
-		$(".nav-selected").data("index", 0);
-
 		// fade in website
 		TweenLite.to($(".wrapper"), 0.5, { opacity: 1 } );
 
 		if($(window).height() > 400) { // desktop animation
-
-			var base = $(".nav-home").position().top;
 
 			// tween to position
 			$(".nav-ball").each(function(i, ball) {
@@ -45,8 +41,6 @@ $( document ).ready( () => {
 			});
 
 		} else { // mobile animation
-
-			var base = $(".nav-home").position().left;
 
 			// tween to position
 			$(".nav-ball").each(function(i, ball) {
@@ -56,6 +50,7 @@ $( document ).ready( () => {
 	}
 
 	// preload images
+	
 	preload( 
 		$(".preload").map(function() { 
 			return $(this).data("image"); } 
@@ -77,29 +72,16 @@ $( document ).ready( () => {
 			expand = true;
 		}
 
-		var selectPos = $(".nav-selected").position();
-		var selectIndex = $(".nav-selected").data("index");
-
 		// tween if necessary
 		if( $(window).height() > 400 && (!desktop || expand) ) {
 			desktop = true;
 
-			TweenLite.to($(".nav-selected"), 0.4, {
-				delay: 0.1 * selectIndex,
-				left: 15, 
-				top: 15+selectIndex*ball_distance
-			});
 			$(".nav-ball").each(function(i, ball) {
 				TweenLite.to(ball, 0.4, {delay:0.1*i, left: 15, top: 15+i*ball_distance});
 			});
 		} else if( $(window).height() <= 400 && (desktop || expand) ){
 			desktop = false;
 
-			TweenLite.to($(".nav-selected"), 0.4, {
-				delay: 0.1 * selectIndex,
-				top: 15, 
-				left: 15+selectIndex*ball_distance
-			});
 			$(".nav-ball").each(function(i, ball) {
 				TweenLite.to(ball, 0.4, {delay:0.1*i, top: 15, left: 15+i*ball_distance});
 			});
@@ -109,26 +91,14 @@ $( document ).ready( () => {
 	// onclicks for navigation
 
 	$(".nav-home").off().click(function() {
-		var position = $(".nav-home").position();
-		$(".nav-selected").data("index", 0);
-		TweenLite.to($(".nav-selected"), 0.25, {top: position.top, left: position.left});
 	});
 
 	$(".nav-about").off().click(function() {
-		var position = $(".nav-about").position();
-		$(".nav-selected").data("index", 1);
-		TweenLite.to($(".nav-selected"), 0.25, {top: position.top, left: position.left});
 	});
 
 	$(".nav-projects").off().click(function() {
-		var position = $(".nav-projects").position();
-		$(".nav-selected").data("index", 2);
-		TweenLite.to($(".nav-selected"), 0.25, {top: position.top, left: position.left});
 	});
 
 	$(".nav-resume").off().click(function() {
-		var position = $(".nav-resume").position();
-		$(".nav-selected").data("index", 3);
-		TweenLite.to($(".nav-selected"), 0.25, {top: position.top, left: position.left});
 	});
 });
